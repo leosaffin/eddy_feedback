31178e76ca4c3308f8d3c645f5660ef394abd9cb
Leo Saffin <l.saffin@leeds.ac.uk>
2023-11-17 09:42:34 +0000

    Simplify plotting box and whisker positions with a gap for pressure-level data

--- eddy_feedback/figures/fig2_efp_era5_bootstrap_errorbars.py
+++ eddy_feedback/figures/fig2_efp_era5_bootstrap_errorbars.py
@@ -27,8 +27,9 @@ def main():
     year_blocks = [(1940, 2022), (1940, 1979), (1979, 2022)]
 
     # Store a list of each result labelled by the year range and pressure levels
-    efp = dict()
-    efp_full = dict()
+    labels = []
+    efp = []
+    efp_full = []
 
     for plevs, suffix in [("500hPa", "NDJFM"), ("600-200hPa", "600-200hPa_DJF")]:
         ep_flux = iris.load_cube(data_path / f"era5_daily_EP-flux-divergence_{suffix}.nc")
@@ -38,7 +39,7 @@ def main():
 
         # Calculate bootstrapped samples of the eddy-feedback parameter over the year ranges
         for year_block in year_blocks:
-            label = "{}-{}\n{}".format(*year_block, plevs)
+            labels.append("{}-{}\n{}".format(*year_block, plevs))
             cs = iris.Constraint(
                 season_year=lambda cell: year_block[0] < cell <= year_block[1]
             )
@@ -46,31 +47,22 @@ def main():
             ep_flux_years = ep_flux.extract(cs)
             u_zm_years = u_zm.extract(cs)
 
-            efp[label] = bootstrap_eddy_feedback_parameter(
+            efp.append(bootstrap_eddy_feedback_parameter(
                 ep_flux_years, u_zm_years, n_resamples
-            )
+            ))
 
             result_full = eddy_feedback_parameter(ep_flux_years, u_zm_years)
             if plevs == "600-200hPa":
                 result_full = result_full.collapsed("pressure_level", MEAN)
-            efp_full[label] = result_full.data
+            efp_full.append(result_full.data)
 
     # Plot box-and-whisker plot for each time period
     plt.figure(figsize=(8, 5))
-    n = 0
-    for label in efp:
-        plt.boxplot(efp[label], positions=[n], whis=(2.5, 97.5))
-        plt.plot(n, efp_full[label], "kx")
-        print(label, efp_full[label])
+    positions = [1, 2, 3, 5, 6, 7]
+    plt.boxplot(efp, positions=positions, whis=(2.5, 97.5))
+    plt.plot(positions, efp_full, "kx")
 
-        n += 1
-
-        # Add a gap between different pressure level calculations
-        if n == 3:
-            n += 1
-
-    plt.xticks(range(3), efp.keys()[:3])
-    plt.xticks(range(4, 7), efp.keys()[3:])
+    plt.xticks(positions, labels)
     plt.xlabel("Sampling Period")
     plt.ylabel("Eddy-Feedback Parameter")
     plt.title("Bootstrap {} samples".format(n_resamples))
