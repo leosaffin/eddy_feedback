4363a137a82c08127ceab2620cbd0f203dc86b35
Leo Saffin <l.saffin@leeds.ac.uk>
2023-12-13 09:49:37 +0000

    Add figures for North Atlantic box-averaged bartropic energy generation rate

---
+++ eddy_feedback/figures/fig4_lefp_in_box_cmip6_vs_nao_and_efp.py
@@ -0,0 +1,85 @@
+from string import ascii_lowercase
+
+import iris
+from iris.analysis import MEAN
+import numpy as np
+import matplotlib.pyplot as plt
+
+from eddy_feedback import plotdir, bootstrapping, local_eddy_feedback_north_atlantic_index
+from eddy_feedback.figures.fig3_efp_vs_nao_cmip6 import markers, get_data
+
+
+def main():
+    months = ["Dec", "Jan", "Feb"]
+    start_year, end_year = 1941, 2022
+
+    months_str = "".join([m[0] for m in months])
+    data = get_data(months_str, "1850-2014")
+
+    fig, axes = plt.subplots(1, 2, figsize=(8, 5), sharey="all")
+    models = sorted(set(data.model), key=lambda x: x.lower())
+    for model in models:
+        data_model = data.loc[data.model == model]
+        print(model)
+
+        # Keep values of G to calculate ensemble mean
+        lefp_all = []
+
+        # Ensemble members
+        for n, row in data_model.iterrows():
+            # Load North-Atlantic box averaged G
+            fname = local_eddy_feedback_north_atlantic_index.output_filename.format(
+                model=row.model, variant=row.variant
+            )
+            try:
+                lefp = iris.load_cube(fname)
+                lefp = lefp.collapsed("season_year", MEAN).data
+                lefp_all.append(lefp)
+                axes[0].plot(row.efp, lefp, markers[model], alpha=0.5)
+                axes[1].plot(row.nao_variance, lefp, markers[model], alpha=0.5)
+            except OSError:
+                print(row.model, row.variant, "has no corresponding data")
+
+        # Ensemble mean
+        if len(lefp_all) > 0:
+            lefp_mean = np.mean(lefp_all)
+            axes[0].plot(
+                np.mean(data_model.efp), lefp_mean, markers[model], mec="k", label=model
+            )
+            axes[1].plot(
+                np.mean(data_model.nao_variance), lefp_mean, markers[model], mec="k"
+            )
+
+    # Add ERA5
+    nao_variance_era5_samples = bootstrapping.bootstrap_nao(
+        start_year=start_year, end_year=end_year, n_samples=1000, months=months, months_str=months_str,
+    )
+    efp_era5_samples = bootstrapping.bootstrap_eddy_feedback_parameter(
+        start_year=start_year, end_year=end_year, n_samples=1000, plevs="500hPa"
+    )
+    lefp_era5_samples = bootstrapping.bootstrap_local_eddy_feedback_parameter(
+        start_year=start_year, end_year=end_year, n_samples=1000, plevs="250hPa",
+    )
+
+    axes[0].plot(efp_era5_samples, lefp_era5_samples, ".k", alpha=0.1)
+    axes[1].plot(nao_variance_era5_samples, lefp_era5_samples, ".k", alpha=0.1)
+
+    axes[0].set_ylabel("Barotropic Energy Generation Rate")
+    axes[0].set_xlabel("Eddy-Feedback Parameter")
+    axes[1].set_xlabel("NAO Variance (hPa)")
+    fig.subplots_adjust(bottom=0.25)
+    fig.legend(loc="center", ncol=4, bbox_to_anchor=(0.5, 0.1))
+
+    for n, ax in enumerate(axes.flatten()):
+        ax.text(0.01, 1.02, f"({ascii_lowercase[n]})", transform=ax.transAxes)
+
+    plt.savefig(plotdir / "fig4_lefp-in-box-cmip6_vs_efp_and_nao")
+    plt.show()
+
+
+if __name__ == '__main__':
+    import warnings
+
+    warnings.filterwarnings("ignore")
+
+    main()
---
+++ eddy_feedback/figures/figA1_lefp_maps.py
@@ -0,0 +1,101 @@
+from string import ascii_lowercase
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cmcrameri
+import iris
+from iris.analysis import MEAN
+import iris.plot as iplt
+import cartopy.crs as ccrs
+
+from eddy_feedback import datadir, plotdir
+from eddy_feedback.nao_variance import season_mean
+from eddy_feedback.local_eddy_feedback_north_atlantic_index import box
+from eddy_feedback.figures import markers
+
+
+def main():
+    fname = str(
+        datadir / "local_eddy_feedback_parameter_data/Monthly_local_EFP_IPSL/"
+                  "G_lat_lon_DJFM_{model}_historical_r*_monthly.nc"
+    )
+
+    projection = ccrs.EqualEarth()
+    vmin = -1.5e-3
+    vmax = 1.5e-3
+    cmap = plt.get_cmap("cmc.vik")
+
+    nrows, ncols = 5, 3
+    fig, axes = plt.subplots(nrows, ncols, figsize=(8, 8), subplot_kw=dict(projection=projection))
+
+    all_models = iris.cube.CubeList()
+    n_models = 0
+    for n, model in enumerate(markers, start=2):
+        print(model)
+        lefp = iris.load(fname.format(model=model))
+        n_members = len(lefp)
+        lefp_all = sum([cube.data for cube in lefp]) / n_members
+        lefp_all = lefp[0].copy(data=lefp_all)
+
+        lefp_all = season_mean(lefp_all, months=["Dec", "Jan", "Feb"], seasons=["ndjfma", "mjjaso"])
+        lefp_all = lefp_all.collapsed("time", MEAN)
+
+        all_models.append(lefp_all * n_members)
+        n_models += n_members
+
+        plt.axes(axes[n // ncols, n % ncols])
+        make_plot(lefp_all, model, n, vmin=vmin, vmax=vmax, cmap=cmap)
+
+    all_models = sum([m.data for m in all_models]) / n_models
+    all_models = lefp_all.copy(data=all_models)
+    plt.axes(axes[0, 1])
+    make_plot(all_models, "All Models", 1, vmin=vmin, vmax=vmax, cmap=cmap)
+
+    plt.axes(axes[0, 0])
+    x = all_models.coord("longitude").points
+    y = all_models.coord("latitude").points
+
+    era5 = iris.load_cube(
+        datadir / "local_eddy_feedback_parameter_data/daily" / "G_mean_MC89_lat_lon_DJF_ERA5.nc",
+        iris.Constraint(month=["Dec", "Jan", "Feb"])
+    )
+    era5 = era5.intersection(longitude=(x.min(), x.max()), latitude=(y.min(), y.max()))
+    print((x.min(), x.max()), (y.min(), y.max()))
+    era5 = era5.extract(iris.Constraint(pressure_level=250))
+    era5 = era5.aggregated_by("season_year", MEAN)
+    era5 = era5.collapsed("time", MEAN)
+
+    im = make_plot(era5, "ERA5", 0, vmin=vmin, vmax=vmax, cmap=cmap)
+
+    fig.subplots_adjust(bottom=0.15)
+    cbar_ax = fig.add_axes([0.05, 0.1, 0.9, 0.015])
+    cbar = fig.colorbar(im, cax=cbar_ax, orientation="horizontal")
+    cbar.set_label("Barotropic energy generation rate")
+
+    plt.savefig(plotdir / "figA1_lefp_NA_cmip6.png")
+    plt.show()
+
+
+def make_plot(lefp, model, n, na_box=(-60, -25, 30, 45), **kwargs):
+    im = iplt.pcolormesh(lefp, **kwargs)
+    ax = plt.gca()
+    ax.coastlines()
+
+    transform = ccrs.PlateCarree()
+    ax.plot([na_box[0], na_box[1]], [na_box[2], na_box[2]], "-k", transform=transform)
+    ax.plot([na_box[1], na_box[1]], [na_box[2], na_box[3]], "-k", transform=transform)
+    ax.plot([na_box[1], na_box[0]], [na_box[3], na_box[3]], "-k", transform=transform)
+    ax.plot([na_box[0], na_box[0]], [na_box[3], na_box[2]], "-k", transform=transform)
+
+    ax.text(-0.1, 1.1, f"({ascii_lowercase[n]})", transform=ax.transAxes)
+    ax.set_title(f"{model}")
+
+    return im
+
+
+if __name__ == '__main__':
+    import warnings
+
+    warnings.filterwarnings("ignore")
+
+    main()
